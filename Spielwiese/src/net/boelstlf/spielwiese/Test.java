e21d696ab8806d7c726d4e541231883321e5d93c
boelstlf <boelstlf@franks-mbp.fritz.box>
2017-10-07 12:46:32 +0200

    # WARNING: head commit changed in the meantime
    new feature
    
    
    Change-Id: I4c621c0e2ae2d1562ae6fd596783ada2bc955d0d
    Signed-off-by: boelstlf <boelstlf@franks-mbp.fritz.box>

---
+++ Spielwiese/src/net/boelstlf/spielwiese/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.boelstlf.spielwiese;
+
+/**
+ * @author boelstlf
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("first test");
+
+	}
+
+}
